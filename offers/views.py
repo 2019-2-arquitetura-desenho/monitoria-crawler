22e48bdabc81f690ef0e8fbd37c3132c1ecaceda
Lieverton <lievertom@gmail.com>
2019-10-01 09:34:52 -0300

    Create views classes
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

--- offers/views.py
+++ offers/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DisciplineSerializer, DisciplineClassSerializer
+from .serializers import ProfessorSerializer, AlocationSerializer, MeetingSerializer
 
-# Create your views here.
+
+class DisciplineViewSet(viewsets.ModelViewSet):
+    queryset = Discipline.objects.all()
+    serializer_class = DisciplineSerializer
+
+
+class DisciplineClassViewSet(viewsets.ModelViewSet):
+    queryset = DisciplineClass.objects.all()
+    serializer_class = DisciplineClassSerializer
+
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+
+class AlocationViewSet(viewsets.ModelViewSet):
+    queryset = Alocation.objects.all()
+    serializer_class = AlocationSerializer
+
+
+class MeetingViewSet(viewsets.ModelViewSet):
+    queryset = Meeting.objects.all()
+    serializer_class = MeetingSerializer
