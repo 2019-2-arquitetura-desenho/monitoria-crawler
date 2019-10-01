2f134331559c1ed231919a51dcac35ffd0728628
Lieverton <lievertom@gmail.com>
2019-10-01 08:46:44 -0300

    Model classes
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

--- offers/models.py
+++ offers/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Discipline(models.Model):
+    name = models.CharField(max_length=250)
+    code = models.IntegerField()
+    department = models.IntegerField()
+    credits = models.SmallIntegerField()
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class DisciplineClass(models.Model):
+    name = models.CharField(max_length=5)
+    vacancies = models.SmallIntegerField()
+    discipline = models.ForeignKey(
+        'Discipline',
+        on_delete=models.CASCADE
+    )
+    shift = models.CharField(max_length=250)
+    teachers = models.ManyToManyField(
+        Professor,
+        through='Alocation',
+        through_fields=('discipline_class', 'professor'),
+    )
+
+
+class Alocation(models.Model):
+    discipline_class = models.ForeignKey(
+        'DisciplineClass', on_delete=models.CASCADE
+    )
+    professor = models.ForeignKey(
+        'Professor',
+        on_delete=models.CASCADE
+    )
+
+
+class Meeting(models.Model):
+    discipline_class = models.ForeignKey(
+        'DisciplineClass',
+        on_delete=models.CASCADE
+    )
+    day = models.CharField(max_length=50)
+    init_hout = models.TimeField()
+    final_hout = models.TimeField()
+    room = models.CharField(max_length=250)
