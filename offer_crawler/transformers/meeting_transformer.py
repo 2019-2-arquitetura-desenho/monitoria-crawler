21dc859662eddbcb7e58db46a3420dcd50318775
Lieverton <lievertom@gmail.com>
2019-10-18 09:25:31 -0300

    Add meeting transformer
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

---
+++ offer_crawler/transformers/meeting_transformer.py
@@ -0,0 +1,31 @@
+from transformer import JsonTransformer
+
+
+class MeetingTransformer(JsonTransformer):
+    pk = 1
+    meetings = []
+
+    def __init__(self, meeting):
+        self.map_meeting = collections.defaultdict(dict)
+        self.meeting = meeting
+
+    def define_model(self) -> None:
+        self.map_meeting["model"] = "offers.meeting"
+
+    def define_pk(self) -> None:
+        self.map_meeting["pk"] = MeetingTransformer.pk
+        MeetingTransformer.pk += 1
+
+    def define_fields(self, meeting) -> None:
+        self.map_meeting["fields"]["day"] = self.meeting.getName()
+        self.map_meeting["fields"]["init_hout"] = self.meeting.getName()
+        self.map_meeting["fields"]["final_hout"] = self.meeting.getName()
+        self.map_meeting["fields"]["room"] = self.meeting.getName()
+        self.map_meeting["fields"]["discipline_class"] = self.meeting.getName()
+        print(self.map_meeting)
+        MeetingTransformer.meetings.append(self.map_meeting)
+    
+    def write_json(self):
+        with open('offers/fixtures/meeting.json', "w") as f:
+            json.dump(MeetingTransformer.meetings, f, indent=4)
+            
