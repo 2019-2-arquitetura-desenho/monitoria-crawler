5fb76a4257e8a9d9a26e421985394145e6731a7f
Lieverton <lievertom@gmail.com>
2019-10-24 16:22:27 -0300

    Add save class
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

--- offer_crawler/savers/save_data.py
+++ offer_crawler/savers/save_data.py
@@ -3,6 +3,7 @@ from offer_crawler.transformers.discipline_transformer import DisciplineTransfor
 from offer_crawler.transformers.discipline_class_transformer import DisciplineClassTransformer
 from offer_crawler.transformers.professor_transformer import ProfessorTransformer
 from offer_crawler.transformers.meeting_transformer import MeetingTransformer
+from offer_crawler.savers.Saver import Saver
 import collections
 
 
@@ -13,40 +14,44 @@ class Mediator:
     def __init__(self):
         self.department = self.build_department()
         self.teachers = collections.defaultdict(int)
-        self.build_disciplines()
+        self.disciplines_json()
+        self.save()
 
     def build_department(self):
         return DepartmentBuilder().buildDepartment()
 
-    def build_disciplines(self):
+    def disciplines_json(self):
         pk = 1
         for discipline in self.department.getDisciplines():
             DisciplineTransformer(discipline).template_offer(discipline)
-            self.build_disciplines_class(pk, discipline)
+            self.disciplines_class_jason(pk, discipline)
             pk += 1
 
-    def build_disciplines_class(self, fk, discipline):
+    def disciplines_class_jason(self, fk, discipline):
         for discipline_class in discipline.getClasses():
             professors = []
             for teacher in discipline_class.getTeachers():
                 if self.teachers[teacher] == 0:
                     self.teachers[teacher] = Mediator.teacher_pk
-                    self.build_teachers(teacher)
+                    self.teachers_json(teacher)
                     professors.append(Mediator.teacher_pk)
                     Mediator.teacher_pk += 1
                 else:
                     professors.append(self.teachers[teacher])
 
-            self.build_meetings(Mediator.discipline_class_pk, discipline_class.getMettings())
+            self.meetings_json(Mediator.discipline_class_pk, discipline_class.getMettings())
             Mediator.discipline_class_pk += 1
 
             DisciplineClassTransformer(discipline_class, fk, professors).template_offer(discipline_class)
 
-    def build_teachers(self, teacher):
+    def teachers_json(self, teacher):
         ProfessorTransformer(teacher).template_offer(teacher)
 
-    def build_meetings(self, fk, meetings):
+    def meetings_json(self, fk, meetings):
         for meeting in meetings:
             MeetingTransformer(fk, meeting).template_offer(meeting)
+
+    def save(self):
+        Saver()
 
 a = Mediator()
