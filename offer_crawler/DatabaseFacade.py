c9f1a770cad8db40319e1fc3fd9b2a66045d6b45
Welison Almeida <welison.almeida.923@gmail.com>
2019-10-24 21:49:04 -0300

    Update facade pattern design.
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

--- offer_crawler/DatabaseFacade.py
+++ offer_crawler/DatabaseFacade.py
@@ -2,7 +2,13 @@ from offer_crawler.builders.DepartmentBuilder import DepartmentBuilder
 from offer_crawler.savers.JsonBuilder import JsonBuilder
 from offer_crawler.savers.Saver import Saver
 
+
+class Facade:
+    def __init__(self):
+        self.department = DepartmentBuilder().buildDepartment()
+        JsonBuilder(self.department)
+        Saver()
+
+
 if __name__ == '__main__':
-    department = DepartmentBuilder().buildDepartment()
-    JsonBuilder(department)
-    Saver()
+    Facade()
