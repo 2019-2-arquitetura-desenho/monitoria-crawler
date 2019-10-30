ca56d7d5fa00dcacb573edbefba9e7de50ed8f6d
Lieverton <lievertom@gmail.com>
2019-10-30 09:44:17 -0300

    remove end points disciplineclass, professor, alocation and meeting
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

--- offers/views.py
+++ offers/views.py
@@ -1,30 +1,8 @@
 from rest_framework import viewsets
-from .serializers import DisciplineSerializer, DisciplineClassSerializer
-from .serializers import ProfessorSerializer, AlocationSerializer, MeetingSerializer
-from .models import Discipline, DisciplineClass
-from .models import Professor, Alocation, Meeting
+from .serializers import DisciplineSerializer
+from .models import Discipline
 
 
 class DisciplineViewSet(viewsets.ModelViewSet):
     queryset = Discipline.objects.all()
     serializer_class = DisciplineSerializer
-
-
-class DisciplineClassViewSet(viewsets.ModelViewSet):
-    queryset = DisciplineClass.objects.all()
-    serializer_class = DisciplineClassSerializer
-
-
-class ProfessorViewSet(viewsets.ModelViewSet):
-    queryset = Professor.objects.all()
-    serializer_class = ProfessorSerializer
-
-
-class AlocationViewSet(viewsets.ModelViewSet):
-    queryset = Alocation.objects.all()
-    serializer_class = AlocationSerializer
-
-
-class MeetingViewSet(viewsets.ModelViewSet):
-    queryset = Meeting.objects.all()
-    serializer_class = MeetingSerializer
